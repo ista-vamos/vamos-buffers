e09d7a1c5dee5978afdaf9182669b3bfd81fb8ed
Marek Chalupa <mchqwerty@gmail.com>
2022-04-21 13:09:21 +0200

    event: rename event_kinds -> events_info

--- event.c
+++ event.c
@@ -19,9 +19,9 @@ struct _ev_kind_rec {
     ev_destroy_fn ev_destroy;
 };
 
-struct _ev_kind_rec *event_kinds = NULL;
-size_t ev_kinds_num = 0;
-size_t ev_kinds_num_allocated = 0;
+static struct _ev_kind_rec *events_info = NULL;
+static size_t ev_kinds_num = 0;
+static size_t ev_kinds_num_allocated = 0;
 
 shm_kind shm_mk_event_kind(const char* name,
                            size_t event_size,
@@ -30,42 +30,42 @@ shm_kind shm_mk_event_kind(const char* name,
     size_t idx = ev_kinds_num++;
     if (ev_kinds_num_allocated < ev_kinds_num) {
         ev_kinds_num += 10;
-        event_kinds = realloc(event_kinds, sizeof(struct _ev_kind_rec) * ev_kinds_num);
-        assert(event_kinds && "Allocation failed");
+        events_info = realloc(events_info, sizeof(struct _ev_kind_rec) * ev_kinds_num);
+        assert(events_info && "Allocation failed");
     }
 
     // FIXME: we leak these right now, create a destructor or use atexit?
-    event_kinds[idx].name = strdup(name);
-    event_kinds[idx].ev_size = event_size;
-    event_kinds[idx].ev_copy = copy_fn;
-    event_kinds[idx].ev_destroy = destroy_fn;
-    assert(event_kinds[idx].name);
+    events_info[idx].name = strdup(name);
+    events_info[idx].ev_size = event_size;
+    events_info[idx].ev_copy = copy_fn;
+    events_info[idx].ev_destroy = destroy_fn;
+    assert(events_info[idx].name);
     return idx + 1;
 };
 
 const char *shm_event_kind_name(shm_kind kind) {
     if (kind > ev_kinds_num)
         return NULL;
-    return event_kinds[kind - 1].name;
+    return events_info[kind - 1].name;
 }
 
 ev_copy_fn shm_event_get_copy_fn(shm_event *ev) {
     shm_kind kind = shm_event_kind(ev);
     if (kind > ev_kinds_num)
         return NULL;
-    return event_kinds[kind - 1].ev_copy;
+    return events_info[kind - 1].ev_copy;
 }
 
 ev_destroy_fn shm_event_get_destroy_fn(shm_event *ev) {
     shm_kind kind = shm_event_kind(ev);
     assert(kind <= ev_kinds_num);
-    return event_kinds[kind - 1].ev_destroy;
+    return events_info[kind - 1].ev_destroy;
 }
 
 size_t shm_event_size(shm_event *ev) {
     shm_kind kind = shm_event_kind(ev);
     assert(kind <= ev_kinds_num);
-    return event_kinds[kind - 1].ev_size;
+    return events_info[kind - 1].ev_size;
 };
 
 shm_eventid shm_event_id(shm_event *event) {
