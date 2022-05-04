4104dec04f6b38bf1d447391d4d9b490d0311b40
Marek Chalupa <mchqwerty@gmail.com>
2022-05-04 17:55:55 +0200

    drfun/test: let foo use a string

--- drfun/test.c
+++ drfun/test.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int foo(int x, int y, int z) {
+int foo(int x, const char *s, int z) {
 	return x + 1;
 }
+
+const char *strs[] = {
+        "ahoj",
+        "tw",
+        "whoo",
+        "hell!ou"
+};
 
 int main(int argc, const char *argv[]) {
     int N = atoi(argv[1]);
     for (int i = 0; i < N; ++i) {
-	int a = foo(i % 500000 ? i+1 : i, i*i, -i);
-	printf("#@%d@#\n", a);
+	int a = foo(i % 500000 ? i+1 : i, strs[i % 4], -i);
     }
     return 0;
 }
