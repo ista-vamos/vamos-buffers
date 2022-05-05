657bd51cad982dbe6682e275da9dfd431f9c6918
Marek Chalupa <mchqwerty@gmail.com>
2022-05-05 21:44:50 +0200

    tests: fix buffer-test
    It depended on a fixed capacity.

--- tests/buffer-test.c
+++ tests/buffer-test.c
@@ -1,16 +1,17 @@
 #include <assert.h>
+#include <stdlib.h>
 #include "shmbuf/buffer.h"
 
 int main(void) {
-    int i;
-    struct buffer *b = initialize_shared_buffer(sizeof(int));
+    size_t i;
+    struct buffer *b = initialize_shared_buffer(sizeof(size_t));
     assert(b);
     assert(buffer_size(b) == 0);
     for (i = 1; i < 101; ++i) {
-        assert(buffer_push(b, &i, sizeof(int)) == true);
+        assert(buffer_push(b, &i, sizeof(size_t)) == true);
     }
     assert(buffer_size(b) == 100);
-    int j;
+    size_t j;
     for (i = 1; i < 101; ++i) {
         assert(buffer_pop(b, &j) == true);
         assert(j == i);
@@ -19,10 +20,10 @@ int main(void) {
 
     // pop k
     for (i = 1; i < 101; ++i) {
-        assert(buffer_push(b, &i, sizeof(int)) == true);
+        assert(buffer_push(b, &i, sizeof(size_t)) == true);
     }
     assert(buffer_size(b) == 100);
-    int num[100];
+    size_t num[100];
     assert(buffer_pop_k(b, num, 101) == false);
     assert(buffer_pop_k(b, num, 100) == true);
     for (i = 0; i < 100; ++i) {
@@ -30,26 +31,28 @@ int main(void) {
     }
 
     // pop k with rotate
-    for (i = 1; i < 1024; ++i) {
-        assert(buffer_push(b, &i, sizeof(int)) == true);
+    const size_t capacity = buffer_capacity(b);
+    for (i = 1; i < capacity + 1; ++i) {
+        assert(buffer_push(b, &i, sizeof(size_t)) == true);
     }
-    assert(buffer_push(b, &i, sizeof(int)) == false);
+    assert(buffer_push(b, &i, sizeof(size_t)) == false);
     assert(buffer_pop(b, &i) == true && i == 1);
     assert(buffer_pop(b, &i) == true && i == 2);
     assert(buffer_pop(b, &i) == true && i == 3);
     assert(buffer_pop(b, &i) == true && i == 4);
-    i = 1024;
-    assert(buffer_push(b, &i, sizeof(int)) == true);
-    i = 1025;
-    assert(buffer_push(b, &i, sizeof(int)) == true);
-    // buffer contains 5, ... 1025
-    assert(buffer_size(b) == 1021);
-    int nums[1021];
-    assert(buffer_pop_k(b, nums, 1022) == false);
-    assert(buffer_pop_k(b, nums, 1021) == true);
-    for (i = 0; i < 1021; ++i) {
+    i = capacity + 1;
+    assert(buffer_push(b, &i, sizeof(size_t)) == true);
+    i = capacity + 2;
+    assert(buffer_push(b, &i, sizeof(size_t)) == true);
+    // buffer contains 5, ... capacity + 1
+    assert(buffer_size(b) == capacity - 2);
+    size_t *nums = malloc(sizeof(size_t)*(capacity-2));
+    assert(buffer_pop_k(b, nums, capacity - 1) == false);
+    assert(buffer_pop_k(b, nums, capacity - 2) == true);
+    for (i = 0; i < capacity - 2; ++i) {
         assert(nums[i] == i + 5);
     }
 
+    free(nums);
     destroy_shared_buffer(b);
 }
