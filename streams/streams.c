a2500d08de0d007a26d62fa7c8a788ff436fc025
Marek Chalupa <mchqwerty@gmail.com>
2022-05-11 10:56:27 +0200

    Fix creating streams
    Take the arguments in the form
    "stream-name:stream-source:param1;param2;..."

--- streams/streams.c
+++ streams/streams.c
@@ -7,6 +7,7 @@
 #include "stream-stdin.h"
 #include "stream-fds.h"
 
+/*
 static const char *get_arg(const char *name, size_t len,
                            int argc, char *argv[]) {
         for (int i = 0; i < argc; ++i) {
@@ -17,28 +18,48 @@ static const char *get_arg(const char *name, size_t len,
         return NULL;
 }
 
-const char *get_first_param_ptr(const char *params) {
+
+const char *get_next_part(const char *params, char out[256]) {
+    const char *part_end = find_next_part(params);
+    if (!part_end)
+        return NULL;
+    strncpy(out, params, part_end - params - 1);
+    return part_end;
+}
+*/
+
+const char *find_next_part(const char *params) {
     params = strchr(params, ':');
     if (!params || *(params + 1) == '\0')
         return NULL;
     return params + 1;
 }
 
-const char *get_next_param(const char *params, char out[256]) {
+const char *get_next_part(const char *params, char out[256], char delim) {
     /* get next file */
     if (!params || *params == '\0')
         return NULL;
 
-    const char *param_end = strchr(params + 1, ';');
+    const char *param_end = strchr(params + 1, delim);
     if (param_end) {
         assert(params - param_end < 256 && "Parameter too long");
-        strncpy(out, params, params - param_end);
+        strncpy(out, params, param_end - params);
         out[params - param_end] = 0;
         return param_end + 1;
     } else {
         strcpy(out, params);
         return NULL;
     }
+}
+static const char *get_params(const char *stream_name,
+                              int argc, char *argv[]) {
+    size_t slen = strlen(stream_name);
+    for (int i = 1; i < argc; ++i) {
+        if (strncmp(stream_name, argv[i], slen) == 0) {
+            return argv[i];
+        }
+    }
+    return NULL;
 }
 
 struct stream_rec {
@@ -53,31 +74,44 @@ struct stream_rec avail_streams[] = {
   {NULL, NULL} /* to mark the end */
 };
 
-shm_stream *shm_stream_create(const char *name,
+shm_stream *shm_stream_create(const char *stream_name,
                               const char *signature,
                               int argc,
                               char *argv[]) {
     (void)signature;
-    if (strncmp(name, "calls", 5) == 0) {
-        const char *params = get_arg("calls:", 6, argc, argv);
-        if (!params) {
-            fprintf(stderr, "error: stream 'calls' takes as the parameter the key to SHM\n");
+
+    const char *params = get_params(stream_name, argc, argv);
+    if (!params) {
+        fprintf(stderr, "error: did not find spec for stream '%s'\n", stream_name);
+        return NULL;
+    }
+
+    const char *next = find_next_part(params); /* skip the name */
+    if (!next) {
+        fprintf(stderr, "error: no source specified for stream with name '%s'\n", stream_name);
+        return NULL;
+    }
+
+    char source[256] = {0};
+    next = get_next_part(next, source, ':');
+    if (source[0] == 0) {
+        fprintf(stderr, "error: invalid source specified\n");
+        return NULL;
+    }
+
+    if (strncmp(source, "calls", 5) == 0) {
+        if (!next || *next == 0) {
+            fprintf(stderr, "error: source 'calls' needs the key to SHM as parameter\n");
             return NULL;
         }
-
         char key[256];
-        params = get_first_param_ptr(params);
-        if (!params) {
-            fprintf(stderr, "error: stream 'calls' needs some parameters\n");
+        next = get_next_part(next, key, ';');
+        if (next) {
+            fprintf(stderr, "warning: source 'calls' takes only one parameter, ignoring others\n");
             return NULL;
         }
-        params = get_next_param(params, key);
-        shm_stream *s = shm_create_funs_stream(key);
-        if (get_next_param(params, key)) {
-            fprintf(stderr, "warning: stream 'calls' takes just one parameter, ignoring the rest\n");
-        }
-        return s;
 
+        return shm_create_funs_stream(key);
     }
 #if 0
     else if (strncmp(name, "files", 5) == 0) {
@@ -88,7 +122,7 @@ shm_stream *shm_stream_create(const char *name,
         }
 
         char file[256];
-        params = get_first_param_ptr(params);
+        params = get_next_part(params);
         if (!params) {
             fprintf(stderr, "error: stream 'files' needs some parameters\n");
             return NULL;
