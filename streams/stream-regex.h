9814e55dcba8921f42641a71365c614bc6479520
Marek Chalupa <mchqwerty@gmail.com>
2022-05-11 21:18:12 +0200

    Follow-up for removing the line from regex source

--- streams/stream-regex.h
+++ streams/stream-regex.h
@@ -7,7 +7,6 @@
 
 typedef struct _shm_event_regex {
 	shm_event base;
-    size_t line;
     /* the event arguments */
     unsigned char args[];
 } shm_event_regex;
@@ -15,10 +14,6 @@ typedef struct _shm_event_regex {
 typedef struct _shm_stream_sregex {
 	shm_stream base;
     struct buffer *shmbuffer;
-    /*
-    struct call_event_spec *events;
-    size_t spec_count;
-    */
 } shm_stream_sregex;
 
 shm_stream *shm_create_sregex_stream(const char *key,
