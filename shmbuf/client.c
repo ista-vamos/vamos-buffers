0eae17d9b952b689dfb3613ff6764d521edfa70a
Marek Chalupa <mchqwerty@gmail.com>
2022-08-22 10:53:18 +0200

    shmbuf/client: catch iterruptions

--- shmbuf/client.c
+++ shmbuf/client.c
@@ -2,6 +2,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include "buffer.h"
 #include "client.h"
@@ -9,15 +10,42 @@
 #include "utils.h"
 
 #define SLEEP_TIME 1000
+
+static volatile sig_atomic_t interrupted = 0;
+
+static void sig_int(int signo) {
+    (void) signo;
+    interrupted = 1;
+}
+
+static inline void restore_sigfunc(void (*sigfunc)(int)) {
+    if (sigfunc != SIG_ERR) {
+        signal(SIGINT, sigfunc);
+    }
+}
+
 int buffer_wait_for_monitor(struct buffer *buff) {
+    void (*sigfunc)(int);
+    sigfunc = signal(SIGINT, sig_int);
+
     while (!buffer_monitor_attached(buff)) {
         if (sleep_ms(SLEEP_TIME) != 0) {
+            restore_sigfunc(sigfunc);
             return -errno;
         }
+        if (interrupted) {
+            restore_sigfunc(sigfunc);
+            return -EINTR;
+        }
     }
+
+    restore_sigfunc(sigfunc);
+
     /* sleep once more so that the monitor has some time
      * to move to monitoring code */
-    if (sleep_ms(SLEEP_TIME) != 0)
+    if (sleep_ms(SLEEP_TIME) != 0) {
         return -errno;
+    }
+
     return 0;
 }
