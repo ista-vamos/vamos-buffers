0896f5bc8c7edb8e4e9b4d3abb0e5417b708797d
Marek Chalupa <mchqwerty@gmail.com>
2022-08-22 10:41:36 +0200

    core/utils: fix arguments to nanosleep

--- core/utils.c
+++ core/utils.c
@@ -1,10 +1,19 @@
 #include <time.h>
+#include <assert.h>
 #include <unistd.h>
 
 #include "utils.h"
 
 int sleep_ns(uint64_t ns) {
-    struct timespec ts = {.tv_nsec = ns};
+    struct timespec ts = {0};
+    if (ns > 999999999) {
+        ts.tv_sec = ns / 1000000000;
+        ns %= 1000000000;
+    }
+
+    assert (ns <= 999999999);
+    ts.tv_nsec = ns;
+
     return nanosleep(&ts, NULL);
 }
 
