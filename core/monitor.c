d40d1f0da05deae799d729f8fb74d11b160d6117
Marek Chalupa <mchqwerty@gmail.com>
2022-09-27 17:35:04 +0200

    core/monitor: fix after rebase

--- core/monitor.c
+++ core/monitor.c
@@ -78,7 +78,6 @@ size_t shm_monitor_buffer_peek1(shm_monitor_buffer *buffer, void **data) {
     return shm_par_queue_peek1(&buffer->buffer, data);
 }
 
-<<<<<<< HEAD
 void *shm_monitor_buffer_write_ptr_or_null(shm_monitor_buffer *q) {
     assert(!q->finished && "Asking a pointer from a finished buffer");
     return shm_par_queue_write_ptr(&q->buffer);
@@ -87,9 +86,6 @@ void *shm_monitor_buffer_write_ptr_or_null(shm_monitor_buffer *q) {
 void *shm_monitor_buffer_write_ptr(shm_monitor_buffer *q) {
     assert(!q->finished && "Asking a pointer from a finished buffer");
 
-=======
-void *shm_monitor_buffer_write_ptr(shm_monitor_buffer *q) {
->>>>>>> 4140ff3 (core: add shm_monitor_buffer)
     void *ptr = shm_par_queue_write_ptr(&q->buffer);
     if (ptr)
         return ptr;
@@ -106,10 +102,7 @@ void *shm_monitor_buffer_write_ptr(shm_monitor_buffer *q) {
              * of spinning/sleeping? */
         }
 
-<<<<<<< HEAD
         assert(!q->finished && "Asking a pointer from a finished buffer");
-=======
->>>>>>> 4140ff3 (core: add shm_monitor_buffer)
         ptr = shm_par_queue_write_ptr(&q->buffer);
     } while (!ptr);
 
@@ -118,39 +111,28 @@ void *shm_monitor_buffer_write_ptr(shm_monitor_buffer *q) {
 }
 
 void shm_monitor_buffer_write_finish(shm_monitor_buffer *q) {
-<<<<<<< HEAD
     assert(!q->finished && "Asking a pointer from a finished buffer");
-=======
->>>>>>> 4140ff3 (core: add shm_monitor_buffer)
     shm_par_queue_write_finish(&q->buffer);
 }
 
 /* get an event from the stream, block until there is some and return it
  * or return NULL if the stream ended */
-<<<<<<< HEAD
 void *fetch_arbiter_stream(shm_monitor_buffer *buffer) {
-=======
-void *fetch_arbiter_stream(shm_monitor_buffer *buffer, uint64_t timeout) {
->>>>>>> 4140ff3 (core: add shm_monitor_buffer)
     size_t sleep_time = SLEEP_TIME_NS_INIT;
     size_t spinned = 0;
     void *ev;
 
     while (1) {
-        
         /* wait for the event */
         ev = shm_monitor_buffer_top(buffer);
         if (ev) {
             return ev;
         }
 
-<<<<<<< HEAD
         if (buffer->finished) {
             return NULL;
         }
 
-=======
->>>>>>> 4140ff3 (core: add shm_monitor_buffer)
         /* before sleeping, try just to busy wait some time */
         if (spinned < BUSY_WAIT_TIMES) {
             ++spinned;
@@ -162,52 +144,15 @@ void *fetch_arbiter_stream(shm_monitor_buffer *buffer, uint64_t timeout) {
             sleep_time *= 10;
         }
 
-<<<<<<< HEAD
-=======
-        if (timeout > 0) {
-            if (timeout < sleep_time)
-                return NULL;
-            timeout -= sleep_time;
-        }
-
->>>>>>> 4140ff3 (core: add shm_monitor_buffer)
         sleep_ns(sleep_time);
     }
 
     assert(0 && "Unreachable");
 }
 
-<<<<<<< HEAD
 size_t shm_monitor_buffer_consume(shm_monitor_buffer *buffer, size_t k) {
     return shm_par_queue_drop(&buffer->buffer, k);
 }
-=======
-#if 0
-/* drop an event and notify buffer the buffer that it may free up
- * the payload of this and older events */
-size_t shm_monitor_buffer_drop(shm_monitor_buffer *buffer, size_t k) {
-#ifdef DUMP_STATS
-    buffer->volunt_dropped_num_asked += k;
-#endif
-    --k; /* peek_*_at takes index from 0 */
-    shm_event *ev = shm_par_queue_peek_atmost_at(&buffer->buffer, &k);
-    if (!ev)
-        return 0; /* empty queue */
-    shm_eventid last_id = shm_event_id(ev);
-#ifndef NDEBUG
-    size_t n =
-#endif
-        ++k; /* k is index, we must increase it back by one */
-    shm_par_queue_drop(&buffer->buffer, k);
-    assert(n == k && "Something changed the queue in between");
-    shm_stream_notify_last_processed_id(buffer->stream, last_id);
-#ifdef DUMP_STATS
-    buffer->volunt_dropped_num += k;
-#endif
-    return k;
-}
-#endif
->>>>>>> 4140ff3 (core: add shm_monitor_buffer)
 
 /* wait for an event on the 'stream'
 void shm_monitor_buffer_notify_dropped(shm_monitor_buffer *buffer,
