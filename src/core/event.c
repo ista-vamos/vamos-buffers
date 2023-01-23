988ab596c1bfac263d01a817e4788184b5a6be6c
Marek Chalupa <mchqwerty@gmail.com>
2023-01-23 18:54:17 +0100

    core: fix some prototypes

--- src/core/event.c
+++ src/core/event.c
@@ -7,25 +7,25 @@
 static const shm_kind hole_kind = 1;
 static const shm_kind last_special_kind = 1;
 
-void initialize_events() {
+void initialize_events(void) {
     assert(hole_kind == 1 && "We assume that the 'hole_kind' is 1 for now");
     assert(hole_kind > 0 && "Events not initialized");
 }
 
-void deinitialize_events() {}
+void deinitialize_events(void) {}
 
 bool shm_event_is_hole(shm_event *ev) {
     assert(hole_kind > 0);
     return shm_event_kind(ev) == hole_kind;
 }
 
-shm_kind shm_get_hole_kind() {
+shm_kind shm_get_hole_kind(void) {
     assert(hole_kind > 0);
     assert(last_special_kind >= hole_kind);
     return hole_kind;
 }
 
-shm_kind shm_get_last_special_kind() {
+shm_kind shm_get_last_special_kind(void) {
     assert(last_special_kind > 0);
     return last_special_kind;
 }
