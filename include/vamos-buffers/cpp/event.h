a5e68644cd43774d14d4296eb1031365a922ed01
Marek Chalupa <mchqwerty@gmail.com>
2023-04-21 07:19:25 +0200

    Fix format the new commits

--- include/vamos-buffers/cpp/event.h
+++ include/vamos-buffers/cpp/event.h
@@ -7,13 +7,13 @@ namespace vamos {
 
 struct Event : public vms_event {
     Event() {}
-    Event(vms_kind kind, vms_eventid id): _vms_event(kind, id) {}
+    Event(vms_kind kind, vms_eventid id) : _vms_event(kind, id) {}
 
     vms_eventid id() const { return vms_event_id(this); }
     vms_kind kind() const { return vms_event_kind(this); };
     bool is_hole() const { return vms_event_is_hole(this); }
 };
 
-}
+}  // namespace vamos
 
 #endif
